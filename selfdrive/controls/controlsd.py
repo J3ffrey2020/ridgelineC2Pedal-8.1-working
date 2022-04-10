79a47b235154fd0209be7547cf958a34bcae5873
NightriderOP <aragonhedlund0450@gmail.com>
2022-04-10 10:00:00 -0400

    Take 10 off

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -693,7 +693,7 @@ class Controls:
     controlsState.distanceTraveled = self.distance_traveled
 
     if self.v_cruise_kph != 255:
-      controlsState.vCruise = controlsState.vCruise * 1.0355
+      controlsState.vCruise = controlsState.vCruise * 1.0345
     if self.joystick_mode:
       controlsState.lateralControlState.debugState = lac_log
     elif self.CP.steerControlType == car.CarParams.SteerControlType.angle:
