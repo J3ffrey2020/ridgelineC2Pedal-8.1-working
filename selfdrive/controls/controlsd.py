d9453da6901f40f7d60d80308153773077f7d6eb
NightriderOP <aragonhedlund0450@gmail.com>
2022-04-18 10:30:59 -0400

    Offset PCM

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -687,6 +687,10 @@ class Controls:
     controlsState.angleSteers = CS.steeringAngleDeg
     controlsState.steeringAngleDesiredDeg = actuators.steeringAngleDeg
 
+    # 2022 Toyota Camry Hybrid PCM Offset
+    if self.v_cruise_kph != 255:
+      controlsState.vCruise = controlsState.vCruise * 1.0330
+
     if self.joystick_mode:
       controlsState.lateralControlState.debugState = lac_log
     elif self.CP.steerControlType == car.CarParams.SteerControlType.angle:
