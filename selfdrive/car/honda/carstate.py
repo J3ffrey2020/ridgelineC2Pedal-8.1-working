1041b200ea8539faea1299f3311d9b6f9b643f26
Mostafa Ayesh <mostafaayesh@outlook.com>
2022-05-20 00:33:03 -0400

    cruise_setting car state should be assigned later

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -271,7 +271,6 @@ class CarState(CarStateBase):
     ret.steeringAngleDeg = cp.vl["STEERING_SENSORS"]["STEER_ANGLE"]
     ret.steeringRateDeg = cp.vl["STEERING_SENSORS"]["STEER_ANGLE_RATE"]
 
-    self.cruise_setting = cp.vl["SCM_BUTTONS"]["CRUISE_SETTING"]
     self.cruise_buttons = cp.vl["SCM_BUTTONS"]["CRUISE_BUTTONS"]
     ret.cruiseButtons = self.cruise_buttons
 
