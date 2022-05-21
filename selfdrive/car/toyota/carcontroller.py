37eadf379dbecdb31f2b986869993350dc668036
NightriderOP <aragonhedlund0450@gmail.com>
2022-05-20 21:29:40 -0400

    Update carcontroller.py

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -48,7 +48,7 @@ class CarController():
 
     # steer torque
     new_steer = int(round(actuators.steer * CarControllerParams.STEER_MAX))
-    apply_steer = apply_toyota_steer_torque_limits(new_steer, self.last_steer, CS.out.steeringTorqueEps, CarControllerParams)
+    apply_steer = apply_toyota_steer_torque_limits(new_steer, self.last_steer, CS.out.steeringTorqueEps, self.torque_rate_limits)
     self.steer_rate_limited = False
 
     cur_time = frame * DT_CTRL
