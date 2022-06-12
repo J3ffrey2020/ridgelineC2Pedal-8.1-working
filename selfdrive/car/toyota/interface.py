912d7bebdab76e28fce2b3454e21cd000a62fec2
NightriderOP <55203877+NightriderOP@users.noreply.github.com>
2022-06-12 18:30:37 -0400

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -112,13 +112,11 @@ class CarInterface(CarInterfaceBase):
     elif candidate in [CAR.CAMRY, CAR.CAMRYH, CAR.CAMRY_TSS2, CAR.CAMRYH_TSS2]:
       stop_and_go = True
       ret.wheelbase = 2.82448
-      ret.steerRatio = 13.7
+      ret.steerRatio = 15.77
       tire_stiffness_factor = 0.7933
       ret.mass = 3400. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
       if candidate in (CAR.CAMRY_TSS2, CAR.CAMRYH_TSS2):
-        set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_TORQUE=2.4, FRICTION=0.05)
-      else:
-        set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
+      set_lat_tune(ret.lateralTuning, LatTunes.PID_C)
 
     elif candidate in [CAR.HIGHLANDER_TSS2, CAR.HIGHLANDERH_TSS2]:
       stop_and_go = True
